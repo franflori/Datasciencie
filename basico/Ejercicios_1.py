7407d72a319c9756c862419b73356d24c4096967
franflori <francisco.florido@gmail.com>
2022-07-20 12:13:42 +0200

    ejercicio 7

--- basico/Ejercicios_1.py
+++ basico/Ejercicios_1.py
@@ -17,3 +17,10 @@ print(f'el valor de s es {s} y el valor de y es {y}')
 print('el valor de s es ' + str(s) + ' y el valor de y es ' + str(y))
 print('el valor de s es ', s, ' y el valor de y es ', y)
 print(f'el valor de s es %s y el valor de y es %s' %(s, y))
+
+#Ejercicio 7
+#Muestra el máximo y el mínimo de (3, -6, 4, -10, 2.6666)
+
+L = [3, -6, 4, -10, 2.6666]
+print(L)
+print(max(L),min(L))
