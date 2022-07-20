219ea9183a4ecf451b8f0f993c30399e5bb53f8a
franflori <francisco.florido@gmail.com>
2022-07-20 11:46:30 +0200

    segundo ejercicio

--- basico/Ejercicios_1.py
+++ basico/Ejercicios_1.py
@@ -4,3 +4,16 @@
 """
 notas = -3
 print(notas)
+
+#Ejercicio 2
+""""
+
+Imprime los valores de "s" es igual 25, de "y" es igual a 10, poniendo la siguiente salida: El valor de "s" es "valor de s" y el valor de y es
+"valor de y"
+"""
+s = 25
+y = 10
+print(f'el valor de s es {s} y el valor de y es {y}')
+print('el valor de s es ' + str(s) + ' y el valor de y es ' + str(y))
+print('el valor de s es ', s, ' y el valor de y es ', y)
+print(f'el valor de s es %s y el valor de y es %s' %(s, y))
