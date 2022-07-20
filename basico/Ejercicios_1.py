47db0ca585de4ec31712576b08d292add48a14cd
franflori <francisco.florido@gmail.com>
2022-07-20 12:37:52 +0200

    Ejercico 8 se debe pip install pandas instalar

--- basico/Ejercicios_1.py
+++ basico/Ejercicios_1.py
@@ -1,3 +1,5 @@
+#Importar librerias
+
 #Ejercicio 1
 """
     Describe una variable con nombre "notas" que tenga el valor de -3, muestra su valor.
@@ -24,3 +26,28 @@ print(f'el valor de s es %s y el valor de y es %s' %(s, y))
 L = [3, -6, 4, -10, 2.6666]
 print(L)
 print(max(L),min(L))
+
+
+#Ejercicio 8
+"""Tratar de reemplazar los valores que faltan en este listado --> por un -1
+L = [10, None, 8, 5, None, 20]
+1) Hazlo de la forma más fácil posible teniendo en cuenta la posición (index) de esos valores."""
+
+L = [10, None, 8, 5, None, 20]
+L[1] = -1
+L[-2] = -1
+print(L)
+
+""""2) Crea un dataframe con esos valores (L = [10, None, 8, 5, None, 20])"""
+"""
+import pandas as pd
+nuevo=[10, None, 8, 5, None, 20]
+df=pd.DataFrame(nuevo)
+print(df)
+
+
+"""
+import pandas as pd
+nuevo=[10, None, 8, 5, None, 20]
+df=pd.DataFrame(nuevo)
+print(df)
