233beeefab0b28e0c67166373fd48757f56d53e5
franflori <francisco.florido@gmail.com>
2022-09-05 10:36:43 +0200

    subida

---
+++ basico/Ejercicio_5.py
@@ -0,0 +1,47 @@
+# EJERCICIO 1
+
+# a) Declara la variable "test" como una lista con los siguientes componentes: 25, 8, 32, 20, 1.
+    # Usa las formas que conozcas para crearla y el uso de type para asegurarte de que es una lista.
+
+# b) Apendiza un valor de valor 20, 32, 25, 32
+
+# c) Elimina el valor 8 de la lista
+
+# d) Elimina los duplicados que haya en la lista test
+
+# e) Crea una segunda lista de nombre "info" que contenga los valores:
+    #  25, 100, 10, 20, 5, 25, 30, 200
+
+# f) ¿Cuántos valores se repiten entre las listas?
+
+# g) Muéstrame el maximo y mínimo en las dos listas
+
+# h) Crea una nueva variable de nombre "matriz" transformando la lista test en matriz
+
+# i) Crea una función que se llame "funcion_división" donde se divida
+    # el test con valor 32 entre info con valor 5 y muestra el resto de la división
+
+# j) Con ayuda de reverse() muestra la inversa de test
+
+# k) Con el listado info utiliza un bucle for con la ayuda de enumerate(),
+    # para mostrar posición y valor y crea un diccionario de nombre "newDict"
+
+# l) Crea un nuevo listado con nombre "info2" donde los valores: 25 se sustituya por "María",
+    # el valor 20 por "Juan" y el valor 10 por "Pedro"
+
+# m) Sustituye en el listado test los multiplos de 2 por "Dos"
+
+
+
+# EJERCICIO 2
+
+"""
+    Escribe un programa que imprima los números desde 1 hasta 100
+
+    Pero:
+
+    * Para los múltiplos de 3 escribe "Hello"
+    * Para los múltiplos de 5 escribe "World"
+    * Para los múltiplos de ambos (3 y 5) escribe "Hello World"
+    (en vez de los números correspondientes)
+"""
